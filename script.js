b1baf8a90da966e4d4863a4521728094df59a908
Aleksandar Pletl <pletl.aleksandar@gmail.com>
2024-07-02 14:10:59 +0200

    Works fine

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/**
+ * Prisluskuje dogadjaj "scroll" i dodeljuje vrednost globalnoj varijabli "--scroll"
+ * 
+ * The pageYOffset property returns the pixels a document has scrolled from the upper left corner of the window.
+ * The pageYOffset property is equal to the scrollY property.
+ * 
+ * The HTMLElement.offsetHeight read-only property returns the height of an element, including vertical padding and borders, as an integer.
+ * 
+ * The innerHeight property returns the height of a window's content area.
+ */
+
 window.addEventListener(
     "scroll",
     () => {
@@ -10,13 +21,23 @@ window.addEventListener(
 );
 
 
-/**
- * Prisluskuje dogadjaj "scroll" i dodeljuje vrednost globalnoj varijabli "--scroll"
- * 
- * The pageYOffset property returns the pixels a document has scrolled from the upper left corner of the window.
- * The pageYOffset property is equal to the scrollY property.
- * 
- * The HTMLElement.offsetHeight read-only property returns the height of an element, including vertical padding and borders, as an integer.
- * 
- * The innerHeight property returns the height of a window's content area.
+
+
+
+/*
+ * Nadgleda sakrivene elemente, dodaje im klasu show kada se nadju na aktuelnom delu prozora, a hidden kada se odskrola dalje od njih
  */
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
